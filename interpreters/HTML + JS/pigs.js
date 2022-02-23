3031a01ca6aaf31f1233fd752737a2650d67a826
Yousurname <50276952+Y0UR-U5ERNAME@users.noreply.github.com>
2022-02-23 18:19:29 -0500

    Update pigs.js

--- interpreters/HTML + JS/pigs.js
+++ interpreters/HTML + JS/pigs.js
@@ -7,75 +7,76 @@ let interval;
 function run() {
     let i, j, J;
     let b = false;
-    document.getElementById("status").innerHTML = "Running...";
-    if (commands)
-    interval = setInterval(() => {
-        if (c < commands.length) {
-            i = commands[c][0];
-            j = commands[c].substring(1, commands[c].length);
-            j = parseInt(j, 2);
-            initvar(j);
-            J = variables[j];
-            switch (i) {
-                case "P":
-                    try {
-                        output(String.fromCodePoint(J));
-                    } catch {}
-                    break;
-                case "I":
-                    if (inp === undefined) {
-                        if (!waiting) {
-                            output('<textarea class="input" rows="1" cols="5"></textarea><button id="submit" onclick="sub();" onchange="">Submit</button>');
-                            waiting = true;
+    if (commands) {        
+        document.getElementById("status").innerHTML = "Running...";
+        interval = setInterval(() => {
+            if (c < commands.length) {
+                i = commands[c][0];
+                j = commands[c].substring(1, commands[c].length);
+                j = parseInt(j, 2);
+                initvar(j);
+                J = variables[j];
+                switch (i) {
+                    case "P":
+                        try {
+                            output(String.fromCodePoint(J));
+                        } catch {}
+                        break;
+                    case "I":
+                        if (inp === undefined) {
+                            if (!waiting) {
+                                output('<textarea class="input" rows="1" cols="5"></textarea><button id="submit" onclick="sub();" onchange="">Submit</button>');
+                                waiting = true;
+                            }
+                        } else {
+                            for (var k = 0; k < inp.length; k++) {
+                                idx = J + k;
+                                clamp(idx);
+                                variables[idx] = inp.codePointAt(k);
+                            }
+                            inp = undefined;
                         }
-                    } else {
-                        for (var k = 0; k < inp.length; k++) {
-                            idx = J + k;
-                            clamp(idx);
-                            variables[idx] = inp.codePointAt(k);
-                        }
-                        inp = undefined;
-                    }
-                    break;
-                case "G":
-                    c = J - 1;
-                    break;
-                case "S":
-                    pv = J;
-                    initvar(pv);
-                    break;
-                case "+":
-                    variables[pv] += J;
-                    clamp(variables[pv]);
-                    break;
-                case "-":
-                    variables[pv] -= J;
-                    clamp(variables[pv]);
-                    break;
-                case "=":
-                    variables[pv] = Number(variables[pv] === J);
-                    break;
-                case ":":
-                    initvar(J);
-                    variables[pv] = variables[J];
-                    break;
-                case "#":
-                    variables[pv] = j;
-                    clamp(variables[pv]);
-                    break;
-            }
-            if (!waiting) {
-                c++;
+                        break;
+                    case "G":
+                        c = J - 1;
+                        break;
+                    case "S":
+                        pv = J;
+                        initvar(pv);
+                        break;
+                    case "+":
+                        variables[pv] += J;
+                        clamp(variables[pv]);
+                        break;
+                    case "-":
+                        variables[pv] -= J;
+                        clamp(variables[pv]);
+                        break;
+                    case "=":
+                        variables[pv] = Number(variables[pv] === J);
+                        break;
+                    case ":":
+                        initvar(J);
+                        variables[pv] = variables[J];
+                        break;
+                    case "#":
+                        variables[pv] = j;
+                        clamp(variables[pv]);
+                        break;
+                }
+                if (!waiting) {
+                    c++;
+                } else {
+                    b = true;
+                    document.getElementById("status").innerHTML = "Waiting for input...";
+                }
             } else {
                 b = true;
-                document.getElementById("status").innerHTML = "Waiting for input...";
+                document.getElementById("status").innerHTML = "";
             }
-        } else {
-            b = true;
-            document.getElementById("status").innerHTML = "";
-        }
-        if (b) {clearInterval(interval); b = false;}
-    }, 0);
+            if (b) {clearInterval(interval); b = false;}
+        }, 0);
+    }
 }
 
 function sub() {
