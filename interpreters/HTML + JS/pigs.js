dde8e6e75d06e26a12aecb48fee9fd18bdc559d5
Yousurname <50276952+Y0UR-U5ERNAME@users.noreply.github.com>
2022-02-24 20:20:09 -0500

    Update pigs.js

--- interpreters/HTML + JS/pigs.js
+++ interpreters/HTML + JS/pigs.js
@@ -11,66 +11,69 @@ function run() {
         document.getElementById("status").innerHTML = "Running...";
         interval = setInterval(() => {
             if (c < commands.length) {
-                i = commands[c][0];
-                j = commands[c].substring(1, commands[c].length);
-                j = parseInt(j, 2);
-                initvar(j);
-                J = variables[j];
-                switch (i) {
-                    case "P":
-                        try {
-                            output(String.fromCodePoint(J));
-                        } catch {}
-                        break;
-                    case "I":
-                        if (inp === undefined) {
-                            if (!waiting) {
-                                output('<textarea class="input" rows="1" cols="5"></textarea><button id="submit" onclick="sub();">Submit</button>');
-                                waiting = true;
+                while (c < commands.length) {
+                    i = commands[c][0];
+                    j = commands[c].substring(1, commands[c].length);
+                    j = parseInt(j, 2);
+                    initvar(j);
+                    J = variables[j];
+                    switch (i) {
+                        case "P":
+                            try {
+                                output(String.fromCodePoint(J));
+                            } catch {}
+                            break;
+                        case "I":
+                            if (inp === undefined) {
+                                if (!waiting) {
+                                    output('<textarea class="input" rows="1" cols="5"></textarea><button id="submit" onclick="sub();">Submit</button>');
+                                    waiting = true;
+                                }
+                            } else {
+                                var k = 0;
+                                for (var l of inp) {
+                                    var idx = J + k;
+                                    clamp(idx);
+                                    variables[idx] = l.codePointAt(0);
+                                    k++;
+                                }
+                                inp = undefined;
                             }
-                        } else {
-                            var k = 0;
-                            for (var l of inp) {
-                                var idx = J + k;
-                                clamp(idx);
-                                variables[idx] = l.codePointAt(0);
-                                k++;
-                            }
-                            inp = undefined;
-                        }
-                        break;
-                    case "G":
-                        c = J - 1;
-                        break;
-                    case "S":
-                        pv = J;
-                        initvar(pv);
-                        break;
-                    case "+":
-                        variables[pv] += J;
-                        clamp(variables[pv]);
-                        break;
-                    case "-":
-                        variables[pv] -= J;
-                        clamp(variables[pv]);
-                        break;
-                    case "=":
-                        variables[pv] = Number(variables[pv] === J);
-                        break;
-                    case ":":
-                        initvar(J);
-                        variables[pv] = variables[J];
-                        break;
-                    case "#":
-                        variables[pv] = j;
-                        clamp(variables[pv]);
-                        break;
-                }
-                if (!waiting) {
-                    c++;
-                } else {
-                    b = true;
-                    document.getElementById("status").innerHTML = "Waiting for input...";
+                            break;
+                        case "G":
+                            c = J - 1;
+                            break;
+                        case "S":
+                            pv = J;
+                            initvar(pv);
+                            break;
+                        case "+":
+                            variables[pv] += J;
+                            clamp(variables[pv]);
+                            break;
+                        case "-":
+                            variables[pv] -= J;
+                            clamp(variables[pv]);
+                            break;
+                        case "=":
+                            variables[pv] = Number(variables[pv] === J);
+                            break;
+                        case ":":
+                            initvar(J);
+                            variables[pv] = variables[J];
+                            break;
+                        case "#":
+                            variables[pv] = j;
+                            clamp(variables[pv]);
+                            break;
+                    }
+                    if (!waiting) {
+                        c++;
+                    } else {
+                        b = true;
+                        document.getElementById("status").innerHTML = "Waiting for input...";
+                    }
+                    if (i === "G" || b) break;
                 }
             } else {
                 b = true;
